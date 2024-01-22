ffb01a7aa3bde6dd815f0f05a01634963b195920
Ilya Kotovskiy <ilundrik026@gmail.com>
2024-01-22 19:28:52 +0300

    reactivity

---
+++ VueLess_3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <counter></counter>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+
+export default {
+  components: {
+    Counter
+  }
+}
+
+</script>
+
+<style></style>
---
+++ VueLess_3/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{ count }}</p>
+  <button @click="incr(100)">Incr</button>
+  <button @click="console.log(count)">log</button>
+  <button @click="eventHandle(count, $event)">Event Handle</button>
+  <input @keydown="eventHandle(count, $event)" />
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    incr(n) {
+      this.count += n
+    },
+    eventHandle(count, e) {
+      console.log(count, e)
+    }
+  }
+}
+
+</script>
+
+<style></style>
